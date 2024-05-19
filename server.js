1a2058b53feea29af5ae1249e9593610ece71dec
Erica Balderas <ericab40033@hotmail.com>
2024-05-18 21:24:52 -0600

    failed will try again later

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes/index');
+const htmlRoutes = require('./routes/htmlRoutes/index');
 
 app.use(express.static('public'));
 // parse incoming string or array data
